cb9cfe9989309a0229c3366c692fea016b364b81
Rishi Agarwal <23ucs185@lnmiit.ac.in>
2024-05-06 20:54:20 +0530

    can't we commit without a message

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Hi, it a clown for educational purpose");
+
+//const top = document.getElementsById("kio");
+top.addEventListener("click",() => {
+    window.scrollTo({
+        top : 0,
+        left : 0,
+        behavior : "smooth"
+    });
+});
+alert("Hi, it a clown for educational purpose \nJurisdiction Limited to Antarctica Court");
