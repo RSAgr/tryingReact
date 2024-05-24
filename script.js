752fed69057a0de1471b7df7b747520bbefb3207
Rishi Agarwal <23ucs185@lnmiit.ac.in>
2024-05-24 20:11:44 +0530

    improved the surfing

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-
-//const top = document.getElementsById("kio");
-top.addEventListener("click",() => {
+alert("Hi, it a clown for educational purpose \nJurisdiction Limited to Antarctica Court");
+const doi = document.querySelector('.first-panel');
+doi.addEventListener("click",() => {
     window.scrollTo({
         top : 0,
         left : 0,
         behavior : "smooth"
     });
 });
-alert("Hi, it a clown for educational purpose \nJurisdiction Limited to Antarctica Court");
+//onClick="document.getElementById('first').scrollIntoView({ behavior: 'smooth'})";
+//<button onClick="document.getElementById('title').scrollIntoView({ behavior: 'smooth'});"> Back to top </button>
+
